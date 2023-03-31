1cb83f5dfad690babe68c351655424aa95ae8509
teewhy <tyleryeh047@gmail.com>
2023-03-31 20:47:06 +0800

    Updated fzf color scheme

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,13 @@ if status is-interactive
 	set -x LC_ALL en_US.UTF-8
 	set -x LC_CTYPE en_US.UTF-8
 
+	set -gx FZF_DEFAULT_OPTS '
+ 	--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker=">"
+	--color=fg:#4d4d4c,hl:#e68a99
+	--color=fg+:#4d4d4c,bg+:#eeeeee,hl+:#e68a99
+	--color=info:#52799e,prompt:#a87dd4,pointer:#e68a99
+	--color=marker:#52799e,spinner:#52799e,header:#52799e'
+
 	set -gx fzf_preview_file_cmd "bat --style=numbers --color=always --theme OneHalfLight"
 	set -gx LS_COLORS (vivid -m 24-bit generate ~/.config/fish/ls_theme.yml)
 
