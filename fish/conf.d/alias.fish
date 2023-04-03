00fc7be1854e2bc0e749ae3f6f524b2420f40cee
teewhy <tyleryeh047@gmail.com>
2023-04-03 21:16:56 +0800

    Add abbr for git commands

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -7,3 +7,10 @@ alias log='_fzf_search_git_log'
 alias stats='_fzf_search_git_status'
 alias dir='_fzf_search_directory'
 alias hist='_fzf_search_history'
+
+# Abbr for easy access
+abbr gc 'git commit'
+abbr gp 'git push'
+abbr ga 'git add .'
+abbr gb 'git blame'
+
