a905ded7a92130427488115363efd5a5fb151d9c
teewhy <tyleryeh047@gmail.com>
2023-05-06 23:00:28 +0800

    edit: fzf_git_checkout

--- fish/functions/fzf_git_checkout.fish
+++ fish/functions/fzf_git_checkout.fish
@@ -1,4 +1,5 @@
 function fzf_git_checkout
-    git log --pretty=format:'%h %s' | fzf --ansi --preview 'git show --color=always {1}' --preview-window=right:60% | awk '{print $1}' | xargs git checkout
+    set fzf_git_log_format '%C(bold blue)%h%C(reset) - %C(cyan)%ad%C(reset) %C(yellow)%d%C(reset) %C(normal)%s%C(reset)  %C(dim normal)[%an]%C(reset)'
+    git log --no-show-signature --color=always --format=format:$fzf_git_log_format --date=short | fzf --ansi --preview 'git show --color=always {1}' --preview-window=right:60% | awk '{print $1}' | xargs git checkout
 end
 
