e261ecc6b9cfdc3524391fdc7f28c517abfaa5ab
teewhy <tyleryeh047@gmail.com>
2023-05-24 17:12:45 +0800

    add(nvim): catppuccin and barbar config

---
+++ .config/nvim/plugin/catppuccin.lua
@@ -0,0 +1,27 @@
+require("catppuccin").setup {
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        tags = {"underline"},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {
+        latte = {
+               base = "#fafafa",
+            mantle = "#e6e6e6",
+            crust = "#dcdcdc",
+        },
+        frappe = {},
+        macchiato = {},
+        mocha = {},
+    }
+}
