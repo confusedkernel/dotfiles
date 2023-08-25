7edb68fb1dc3001bcdb6daa933eb3b342cfb0806
Léana 江 <leana.jiang@icloud.com>
2023-08-25 09:35:25 +0800

    ref: moved `plugin/` to `after/plugin/`
    This fixes the race condition where `cmp` doesn't work.
    
    After testing, it is confirmed that the `setup()` of cmp
    needs to be called before registering its default capabilities
    in LSP. (This choice of testing comes down to cmp working in buffers
    where no LSPs are attached.)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -48,7 +48,11 @@ require("lazy").setup({
 	{ "romgrk/barbar.nvim",      dependencies = "nvim-web-devicons" },
 	{
 		"hrsh7th/nvim-cmp",
-		dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" }
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip"
+		}
 	},
 	{
 		"ellisonleao/glow.nvim",
