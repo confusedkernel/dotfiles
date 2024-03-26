f56c8431db972591fb52bf12c8fc319a66e26918
teewhy <tyleryeh047@gmail.com>
2024-03-26 15:02:06 +0800

    mod(nvim): new things

---
+++ .config/nvim/after/plugin/nvchad-ui.lua
@@ -0,0 +1,9 @@
+require("nvchad_ui").setup({
+      lazyVim = true,
+      statusline = { enabled = false },
+      theme_toggle = { "catppuccin-latte", "nordic" },
+      nvdash = {
+          load_on_startup = true,
+
+      },
+  })
