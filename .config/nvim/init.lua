1c4d93d133367f07843afc0db5811c5f6472d15a
Léana 江 <leana.jiang+git@icloud.com>
2025-09-11 22:34:56 +0800

    nvim: fix lazy configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,8 +157,7 @@ local plugins = {
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = "nvim-lua/plenary.nvim",
-		"debugloop/telescope-undo.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim" },
 	},
 
 	----------------
