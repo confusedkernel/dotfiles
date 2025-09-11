d8447dd51970f85f6975e10677ef5901e6daa89e
teewhy <tyleryeh047@gmail.com>
2025-09-11 09:14:34 +0800

    feat(nvim): obsidian usage outside of vault

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -4,5 +4,19 @@ require("obsidian").setup({
 			name = "notes",
 			path = "~/documents/la-mente/note",
 		},
+		{
+			name = "no-vault",
+			path = function()
+				return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+			end,
+			overrides = {
+				notes_subdir = vim.NIL,
+				new_notes_location = "current_dir",
+				templates = {
+					folder = vim.NIL,
+				},
+				disable_frontmatter = true,
+			},
+		},
 	},
 })
